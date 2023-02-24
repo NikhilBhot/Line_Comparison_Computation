7854cbc0af42b19f8bbf6e966141febe38e194a4
unknown <bhotnikhil25@gmail.com>
2023-02-24 14:41:08 +0530

    [Nikhil]-Find The Length Of Line

---
+++ CalculateLengthOfLine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Comparison_Computation
+{
+    public class CalculateLengthOfLine
+    {
+       public static double distance(int x1, int y1, int x2, int y2)
+        {
+           double lenghtOFLine = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+
+            return lenghtOFLine;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Line_Comparison_Computation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double lengthOfLine = CalculateLengthOfLine.distance(3, 4, 4, 3);
+            Console.WriteLine(lengthOfLine);
+        }
+    }
+}
